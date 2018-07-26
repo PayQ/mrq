74ff7ba752db4d9aff33f48a6ebe06536d80afae
Valinorcoin <40829117+PayQ@users.noreply.github.com>
2018-07-26 21:18:48 +0300

    dev

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 55622;
+        nRPCPort = 27027;
     }
 };
 static CBaseMainParams mainParams;
@@ -36,7 +36,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = 55644;
+        nRPCPort = 27127;
         strDataDir = "testnet4";
     }
 };
